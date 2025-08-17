fa1692da49b191e3cfbed98cf8a6fc92590f0a6e
adnane-abdallaoui <adnane.abdallaoui@ump.ac.ma>
2025-08-17 22:37:27 +0100

    modif sur login

--- SQLI_Collabora_FrontEnd/src/app/app-routing.module.ts
+++ SQLI_Collabora_FrontEnd/src/app/app-routing.module.ts
@@ -23,8 +23,7 @@ const routes: Routes = [
       { path: '', component: MainDashComponent },
       { path: 'profile', component: ProfileComponent },
      ]
-    
-    }
+        }
 
 ];
 
--- SQLI_Collabora_FrontEnd/src/app/modules/dashboard-page/dashboard-page.component.ts
+++ SQLI_Collabora_FrontEnd/src/app/modules/dashboard-page/dashboard-page.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class DashboardPageComponent {
   isSidebarOpen = true;
   
-    toggleSidebar() {
+  toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
